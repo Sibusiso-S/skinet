a7aedbf3bd2685d913f18563bba7c856b91462b5
Sibu <sibusisowork@gmail.com>
2022-07-15 19:10:25 +0200

    Add CORS configuration

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,50 +6,60 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
-	public class Startup
-	{
-		private readonly IConfiguration _config;
-		public Startup(IConfiguration config)
-		{
-			_config = config;
-		}
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddAutoMapper(typeof(MappingProfiles));
-			services.AddControllers();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddControllers();
 
-			services.AddDbContext<StoreContext>(x =>
-				x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext>(x =>
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-			services.AddApplicationServices();
-			services.AddSwaggerDocumentation();
-		}
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseMiddleware<ExceptionMiddleware>();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("https://localhost:4200");
+                });
+            });
+        }
 
-			app.UseSwaggerDocumentation();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
 
-			app.UseStatusCodePagesWithReExecute("/errors/{0}");
+            app.UseSwaggerDocumentation();
 
-			app.UseHttpsRedirection();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-			app.UseRouting();
+            app.UseHttpsRedirection();
 
-			app.UseStaticFiles();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseStaticFiles();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
