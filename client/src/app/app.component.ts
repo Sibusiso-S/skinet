642ac40aafcc72cb3ac9b4fa38b5341bdd64e4d9
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-07-26 21:28:58 +0200

    Import shop module into app module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,18 +10,8 @@ import { IProduct } from './core/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-        this.products = response.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
