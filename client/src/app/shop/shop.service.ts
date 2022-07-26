06c475ea967af0143308664d5c661f42f7b58f6d
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-07-26 21:28:46 +0200

    Configure shop module

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPagination } from '../core/models/pagination';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  basesUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<IPagination>(this.basesUrl + 'products?pageSize=50');
+  }
 }
