f0e37273e5fede2a3da648ac9086bfa50226fc6d
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-08-02 21:37:59 +0200

    Define shopping interface

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../core/models/brand';
 import { IPagination } from '../core/models/pagination';
+import { IType } from '../core/models/productType';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +14,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<IPagination>(this.basesUrl + 'products?pageSize=50');
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.basesUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.basesUrl + 'products/types');
   }
 }
