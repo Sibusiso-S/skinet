f7957332b2017ef7d2c92ea6caa19efb236c5ae5
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-10-04 13:42:29 +0200

    Add identity entities

---
+++ Core/Entities/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Entities.Identity
+{
+	public class AppIdentityDbContext : IdentityDbContext
+	{
+		public AppIdentityDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+	}
+}
