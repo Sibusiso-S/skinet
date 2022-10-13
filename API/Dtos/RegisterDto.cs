2437ed50091566674e9a418e17e53c5cf02aaf69
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-10-13 17:21:17 +0200

    Create account Dtos

---
+++ API/Dtos/RegisterDto.cs
@@ -0,0 +1,12 @@
+namespace API.Dtos
+{
+	public class RegisterDto
+	{
+		public string Email { get; set; }
+
+		public string Displayname { get; set; }
+
+		public string Password { get; set; }
+
+	}
+}
