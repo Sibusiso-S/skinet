6d34018ff36ce44c4ff6fef99a4cb3596497290f
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-11-03 20:29:25 +0200

    Add dropdown to modules and html

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -6,13 +6,14 @@ import { PagerComponent } from './components/pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
+    BsDropdownModule.forRoot(),
     ReactiveFormsModule,
   ],
   exports: [
@@ -22,6 +23,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
+    BsDropdownModule,
   ],
 })
 export class SharedModule {}
