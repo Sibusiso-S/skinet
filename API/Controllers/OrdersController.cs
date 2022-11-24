e7f908ac819fffdc111bb4c0f192bb4a32e0eefe
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-11-24 20:53:07 +0200

    Implement endpoint for order creation

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,3 +1,8 @@
+using System.Security.Claims;
+using API.Dtos;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -7,8 +12,10 @@ namespace API.Controllers
 	public class OrdersController : BaseApiController
 	{
 		private readonly IOrderService _orderService;
-		public OrdersController(IOrderService orderService)
+		private readonly IMapper _mapper;
+		public OrdersController(IOrderService orderService, IMapper mapper)
 		{
+			_mapper = mapper;
 			_orderService = orderService;
 
 		}
@@ -16,7 +23,19 @@ namespace API.Controllers
 		[HttpPost]
 		public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
 		{
+			var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
+			var address = _mapper.Map<AddressDto, Address>(orderDto.ShipToAddress);
+
+			var order = await _orderService.CreateOrderAsync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+
+			if (order == null)
+			{
+				return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+			}
+
+			return Ok(order);
 		}
 	}
 }
