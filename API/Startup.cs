d63535682b204ec1b7370fb43ac2597a57161da1
Sibusiso Sibanyoni <sibusiso.sibanyoni1@angloamerican.com>
2022-12-13 00:34:09 +0200

    Section 19

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,7 @@ namespace API
 			{
 				opt.AddPolicy("CorsPolicy", policy =>
 				{
-					policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("https://localhost:4200");
+					policy.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
 				});
 			});
 		}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -10,6 +10,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { StepperComponent } from './components/stepper/stepper.component';
     OrderTotalsComponent,
     TextInputComponent,
     StepperComponent,
+    BasketSummaryComponent,
   ],
   imports: [
     CommonModule,
@@ -26,6 +29,7 @@ import { StepperComponent } from './components/stepper/stepper.component';
     BsDropdownModule.forRoot(),
     ReactiveFormsModule,
     CdkStepperModule,
+    RouterModule,
   ],
   exports: [
     PaginationModule,
@@ -37,6 +41,8 @@ import { StepperComponent } from './components/stepper/stepper.component';
     BsDropdownModule,
     TextInputComponent,
     CdkStepperModule,
+    StepperComponent,
+    BasketSummaryComponent,
   ],
 })
 export class SharedModule {}
